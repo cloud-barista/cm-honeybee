a2eeff7336476e67ddf6886adaf62bcdbff0d35d
ish <ish@innogrid.com>
2023-11-23 16:05:34 +0900

    config: Implement of checking config file

--- lib/config/cm-honeybee-agent.go
+++ lib/config/cm-honeybee-agent.go
@@ -6,8 +6,11 @@ import (
 	"github.com/cloud-barista/cm-honeybee/common"
 	"github.com/jollaman999/utils/fileutil"
 	"gopkg.in/yaml.v3"
+	"net"
+	"net/netip"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -25,6 +28,49 @@ type cmHoneybeeConfig struct {
 
 var CMHoneybeeConfig cmHoneybeeConfig
 var cmHoneybeeConfigFile = "cm-honeybee.yaml"
+
+func checkCMHoneybeeAgentConfigFile() error {
+	if CMHoneybeeConfig.CMHoneybeeAgent.Server.Address == "" {
+		return errors.New("config error: server.address is empty")
+	}
+
+	addrSplit := strings.Split(CMHoneybeeConfig.CMHoneybeeAgent.Server.Address, ":")
+	if len(addrSplit) < 2 {
+		return errors.New("config error: invalid server.address must be {IP or IPv6 or Domain}:{Port} form")
+	}
+	port, err := strconv.Atoi(addrSplit[len(addrSplit)-1])
+	if err != nil || port < 1 || port > 65535 {
+		return errors.New("config error: server.address has invalid port value")
+	}
+	addr, _ := strings.CutSuffix(CMHoneybeeConfig.CMHoneybeeAgent.Server.Address, ":"+strconv.Itoa(port))
+	_, err = netip.ParseAddr(addr)
+	if err != nil {
+		_, err = net.LookupIP(addr)
+		if err != nil {
+			return errors.New("config error: server.address has invalid address value " +
+				"or can't find the domain (" + addr + ")")
+		}
+	}
+
+	if CMHoneybeeConfig.CMHoneybeeAgent.Server.Timeout == "" {
+		return errors.New("config error: server.timeout is empty")
+	}
+
+	timeout, err := strconv.Atoi(CMHoneybeeConfig.CMHoneybeeAgent.Server.Timeout)
+	if err != nil || timeout < 1 {
+		return errors.New("config error: server.timeout has invalid value")
+	}
+
+	if CMHoneybeeConfig.CMHoneybeeAgent.Listen.Port == "" {
+		return errors.New("config error: listen.port is empty")
+	}
+	port, err = strconv.Atoi(CMHoneybeeConfig.CMHoneybeeAgent.Listen.Port)
+	if err != nil || port < 1 || port > 65535 {
+		return errors.New("config error: listen.port has invalid value")
+	}
+
+	return nil
+}
 
 func readCMHoneybeeAgentConfigFile() error {
 	common.RootPath = os.Getenv(common.ModuleROOT)
@@ -61,6 +107,11 @@ func readCMHoneybeeAgentConfigFile() error {
 	}
 
 	err = yaml.Unmarshal(data, &CMHoneybeeConfig)
+	if err != nil {
+		return err
+	}
+
+	err = checkCMHoneybeeAgentConfigFile()
 	if err != nil {
 		return err
 	}
