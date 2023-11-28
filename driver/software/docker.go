a47fab0f255421fb5094b7d25e7e62d8a49997ee
ish <ish@innogrid.com>
2023-11-28 16:07:27 +0900

    software: docker: Check if using socket is a real Docker
    Prevent of showing duplicated information with Podman where socket path is same as Docker.

--- driver/software/docker.go
+++ driver/software/docker.go
@@ -5,13 +5,39 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/jollaman999/utils/logger"
+	"strings"
 )
+
+func isRealDocker(cli *client.Client) (bool, error) {
+	info, err := cli.Info(context.Background())
+	if err != nil {
+		logger.Print(logger.ERROR, true, "DOCKER: Failed to get information of the docker.")
+		return false, err
+	}
+
+	initBinary := info.InitBinary
+	if strings.Contains(strings.ToLower(initBinary), "docker") {
+		return true, nil
+	}
+
+	return false, nil
+}
 
 func GetDockerContainers() ([]types.Container, error) {
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		logger.Print(logger.DEBUG, true, "DOCKER: "+err.Error())
 		return []types.Container{}, err
+	}
+
+	yes, err := isRealDocker(cli)
+	if err != nil {
+		logger.Print(logger.ERROR, true, "DOCKER: "+err.Error())
+		return []types.Container{}, err
+	}
+	if !yes {
+		logger.Print(logger.INFO, true, "DOCKER: Docker not found.")
+		return []types.Container{}, nil
 	}
 
 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
