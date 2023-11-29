a9560425075022c5f7e072def9a0e1cee728d6e1
ish <ish@innogrid.com>
2023-11-29 10:12:11 +0900

    drm: Do not check 0.0.0 version
    nvidia-drm shows as 0.0.0
    
    $ sudo dmesg | grep -i drm
    [    5.326956] systemd[1]: Starting modprobe@drm.service - Load Kernel Module drm...
    [    5.376246] ACPI: bus type drm_connector registered
    [    5.378060] systemd[1]: modprobe@drm.service: Deactivated successfully.
    [    5.378159] systemd[1]: Finished modprobe@drm.service - Load Kernel Module drm.
    [    6.261301] [drm] [nvidia-drm] [GPU ID 0x00000100] Loading driver
    [    7.092481] [drm] Initialized nvidia-drm 0.0.0 20160202 for 0000:01:00.0 on minor 0

--- gpu/drm/drm.go
+++ gpu/drm/drm.go
@@ -3,7 +3,6 @@ package drm
 import (
 	"github.com/NeowayLabs/drm"
 	"github.com/jollaman999/utils/logger"
-	"github.com/pkg/errors"
 	"strconv"
 )
 
@@ -19,11 +18,6 @@ func GetDRMInfo() (DRM, error) {
 	if err != nil {
 		logger.Println(logger.DEBUG, true, "DRM: DRM is not available.")
 		return DRM{}, nil
-	}
-	if v.Major == 0 && v.Minor == 0 && v.Patch == 0 {
-		errMsg := "DRM: failed to get driver version"
-		logger.Println(logger.DEBUG, true, errMsg)
-		return DRM{}, errors.New(errMsg)
 	}
 
 	d := DRM{
