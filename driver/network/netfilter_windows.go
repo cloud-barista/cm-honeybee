e7708d646c01b407f0a2e39eab492f6aecdc7ca2
ish <ish@innogrid.com>
2023-12-07 09:50:07 +0900

    network: netfilter: Do not return error in Windows

--- driver/network/netfilter_windows.go
+++ driver/network/netfilter_windows.go
@@ -5,10 +5,9 @@
 package network
 
 import (
-	"errors"
 	"github.com/cloud-barista/cm-honeybee/model/network"
 )
 
 func GetNetfilterList() (network.Netfilter, error) {
-	return network.Netfilter{}, errors.New("getting netfilter information is only for Linux")
+	return network.Netfilter{}, nil
 }
