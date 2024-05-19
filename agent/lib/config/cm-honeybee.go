ed5f15831c7a0643a86b13c73830ccf72e35caec
ish <ish@innogrid.com>
2024-05-19 16:10:25 +0900

    agent: Remove server configuration

--- agent/lib/config/cm-honeybee.go
+++ agent/lib/config/cm-honeybee.go
@@ -6,8 +6,6 @@ import (
 	"github.com/cloud-barista/cm-honeybee/agent/common"
 	"github.com/jollaman999/utils/fileutil"
 	"gopkg.in/yaml.v3"
-	"net"
-	"net/netip"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -16,10 +14,6 @@ import (
 
 type cmHoneybeeAgentConfig struct {
 	CMHoneybeeAgent struct {
-		Server struct {
-			Address string `yaml:"address"`
-			Timeout string `yaml:"timeout"`
-		} `yaml:"server"`
 		Listen struct {
 			Port string `yaml:"port"`
 		} `yaml:"listen"`
@@ -30,41 +24,10 @@ var CMHoneybeeAgentConfig cmHoneybeeAgentConfig
 var cmHoneybeeAgentConfigFile = "cm-honeybee-agent.yaml"
 
 func checkCMHoneybeeAgentConfigFile() error {
-	if CMHoneybeeAgentConfig.CMHoneybeeAgent.Server.Address == "" {
-		return errors.New("config error: cm-honeybee-agent.server.address is empty")
-	}
-
-	addrSplit := strings.Split(CMHoneybeeAgentConfig.CMHoneybeeAgent.Server.Address, ":")
-	if len(addrSplit) < 2 {
-		return errors.New("config error: invalid cm-honeybee-agent.server.address must be {IP or IPv6 or Domain}:{Port} form")
-	}
-	port, err := strconv.Atoi(addrSplit[len(addrSplit)-1])
-	if err != nil || port < 1 || port > 65535 {
-		return errors.New("config error: cm-honeybee-agent.server.address has invalid port value")
-	}
-	addr, _ := strings.CutSuffix(CMHoneybeeAgentConfig.CMHoneybeeAgent.Server.Address, ":"+strconv.Itoa(port))
-	_, err = netip.ParseAddr(addr)
-	if err != nil {
-		_, err = net.LookupIP(addr)
-		if err != nil {
-			return errors.New("config error: cm-honeybee-agent.server.address has invalid address value " +
-				"or can't find the domain (" + addr + ")")
-		}
-	}
-
-	if CMHoneybeeAgentConfig.CMHoneybeeAgent.Server.Timeout == "" {
-		return errors.New("config error: cm-honeybee-agent.server.timeout is empty")
-	}
-
-	timeout, err := strconv.Atoi(CMHoneybeeAgentConfig.CMHoneybeeAgent.Server.Timeout)
-	if err != nil || timeout < 1 {
-		return errors.New("config error: cm-honeybee-agent.server.timeout has invalid value")
-	}
-
 	if CMHoneybeeAgentConfig.CMHoneybeeAgent.Listen.Port == "" {
 		return errors.New("config error: cm-honeybee-agent.listen.port is empty")
 	}
-	port, err = strconv.Atoi(CMHoneybeeAgentConfig.CMHoneybeeAgent.Listen.Port)
+	port, err := strconv.Atoi(CMHoneybeeAgentConfig.CMHoneybeeAgent.Listen.Port)
 	if err != nil || port < 1 || port > 65535 {
 		return errors.New("config error: cm-honeybee-agent.listen.port has invalid value")
 	}
