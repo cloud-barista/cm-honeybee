f853d151b0d6d95240e8efe1bb6b4027369607fd
임수현 <ish@innogrid.com>
2026-07-13 16:15:49 +0900

    agent, server: Update swagger docs

--- agent/pkg/api/rest/docs/docs.go
+++ agent/pkg/api/rest/docs/docs.go
@@ -3640,21 +3640,6 @@ const docTemplate = `{
                 1048576,
                 524288,
                 2401763328,
-                511,
-                2147483648,
-                1073741824,
-                536870912,
-                268435456,
-                134217728,
-                67108864,
-                33554432,
-                16777216,
-                8388608,
-                4194304,
-                2097152,
-                1048576,
-                524288,
-                2401763328,
                 511
             ],
             "x-enum-comments": {
@@ -3768,8 +3753,6 @@ const docTemplate = `{
                 1000000000,
                 60000000000,
                 3600000000000,
-                -9223372036854775808,
-                9223372036854775807,
                 1,
                 1000,
                 1000000,
@@ -3779,8 +3762,7 @@ const docTemplate = `{
                 1,
                 1000,
                 1000000,
-                1000000000,
-                60000000000
+                1000000000
             ],
             "x-enum-varnames": [
                 "minDuration",
@@ -3815,8 +3797,6 @@ const docTemplate = `{
                 "Second",
                 "Minute",
                 "Hour",
-                "minDuration",
-                "maxDuration",
                 "Nanosecond",
                 "Microsecond",
                 "Millisecond",
@@ -3826,8 +3806,7 @@ const docTemplate = `{
                 "Nanosecond",
                 "Microsecond",
                 "Millisecond",
-                "Second",
-                "Minute"
+                "Second"
             ]
         },
         "v1.Descriptor": {
